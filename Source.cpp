5b5979266e9779407064571c84410b644f5b0e34
NizarDesktop <nizarch22@gmail.com>
2024-04-13 19:29:47 +0300

    Cleaned up Source.cpp.

--- Source.cpp
+++ Source.cpp
@@ -1,46 +1,56 @@
 #include <iostream>
 #include "DES.h"
 
-unsigned int P0[] = 
-{	
-	2,3,4,5,10,
-	9,8,7,6,1
-};
 
-void printMatrixarr(unsigned int* P, int y, int x)
-{
-	for (int i = 0; i < y; i++)
-	{
-		for (int j = 0; j < x; j++)
-		{
-			std::cout << P[i*x+j] << ",";
-		}
-		std::cout << "\n";
-	}
-	std::cout << "Matrix array printed.\n";
-}
 int main()
 {
-	uint64_t input = 225;
+	// Testing the speed and timing of the DES algorithm with key extension (pre feistel loop calculated round keys).
+	// The calculation time of round keys is still included!
 
-	// equiv matrix test
-	//uint64_t input1 = 225;
-	//int a = bEqualMatrix(input, input1, 64);
-	//std::cout << "Result from a: " << a << "\n";
+	clock_t beginning = clock();
+	const int numTests = 524288; // number of tests to generate 4MB of plaintext.
+	uint64_t roundKeys[16];
+	uint64_t plaintext;
+	uint64_t encryption, decryption;
+	uint64_t* plaintexts = new uint64_t[numTests];
+	int bFlag = 0;
 
-	// calculate reverseIP:
-	//printMatrixarr(IP, 8, 8);
-	//unsigned int RIP[64];
-	//for (int i = 0; i < 64; i++)
-	//{
-	//	RIP[IP[i]-1] = i + 1;
-	//}
-	//printMatrixarr(RIP, 8, 8);
-	//printMatrix(input, 2, 5);
+	// looad plaintexts
+	for (int i = 0; i < numTests; i++)
+	{
+		plaintexts[i] = ((uint64_t)rand()) << 32 | rand();
+	}
 
-	//unsigned int P[2][5] = { {2,3,4,5,10},{9,8,7,6,1} };
+	// running tests on Encryption/Decryption validation on random values of plaintext.
+	clock_t start = clock();
+	for (int i = 0; i < numTests; i++)
+	{
+		plaintext = plaintexts[i];
+		InitKeysDES(&roundKeys[0]);
+		EncryptDES(plaintext, &roundKeys[0], encryption);
+		DecryptDES(encryption, &roundKeys[0], decryption);
 
-	//permuteMatrix(&input, (unsigned int*)P, sizeof(P1)/sizeof(unsigned int));
-	//printMatrix(input, 2, 5);
-	foo();
+		if (plaintext != decryption)
+		{
+			bFlag = 1;
+			break;
+		}
+	}
+	clock_t end = clock();
+	double timeDiff = (double)(end - start) / (CLOCKS_PER_SEC);
+
+	double sizeBytes = numTests * 8; // 8 bytes of plaintext
+	double avgTime = timeDiff / numTests;
+	double sizeMegaBytes = sizeBytes / 1048576;
+	double speed = sizeMegaBytes / (timeDiff);
+
+	std::cout << "Was encryption/decryption successful? " << (bFlag ? "false" : "true") << "\n";
+	std::cout << "Total time to encrypt + decrypt: " << timeDiff << "s\n";
+	std::cout << "Total bytes (encrypted+decrypted): " << sizeBytes << " bytes\n";
+	std::cout << "Average time to encrypt + decrypt: " << (timeDiff * 1000 * 1000) / numTests << "us\n";
+	std::cout << "Average speed to encrypt + decrypt: " << speed << "MBPS (Megabytes Per Second)\n";
+	std::cout << "Average speed to encrypt + decrypt: " << speed * 8 << "MbPS (Megabits Per Second)\n";
+
+	double totalTime = (double)(end - beginning) / (CLOCKS_PER_SEC);
+	std::cout << "Total time from start to finish:" << totalTime << "s\n";
 }
